2e7e92a8c8f9165e521c1a55062626d08f2c6773
Germán Martín <github@gmartin.net>
2025-05-23 10:31:33 +0200

    Remove debug echo statements from merge.sh for cleaner output

--- merge.sh
+++ merge.sh
@@ -10,10 +10,6 @@ esp32_dir="heltec_wifi_lora_32"
 esp32s3_dir="heltec_wifi_lora_32_V3"
 esp32c3_dir="esp32c3"
 
-echo ${esp32_dir}
-echo ${esp32s3_dir}
-echo ${esp32c3_dir}
-
 platformio run -e ${esp32_dir} -e ${esp32s3_dir} -e ${esp32c3_dir}
 
 esptool.py --chip esp32 merge_bin -o esp32_${VERSION}_merged.bin --flash_mode dio --flash_size 4MB \
@@ -30,3 +26,4 @@ esptool.py --chip esp32-c3 merge_bin -o esp32_c3_${VERSION}_merged.bin --flash_m
   0x1000  ${build_dir}/${esp32c3_dir}/bootloader.bin \
   0x8000  ${build_dir}/${esp32c3_dir}/partitions.bin \
   0x10000 ${build_dir}/${esp32c3_dir}/firmware.bin
+  
